47708ac6900dc214fadece141fde48d575cf6464
Marco <msoudre@msn.com>
2024-10-05 19:07:36 -0300

    Actualización de Clases

--- Estructura_De_Datos/src/Alumno.java
+++ Estructura_De_Datos/src/Alumno.java
@@ -1,2 +1,22 @@
 public class Alumno {
+    public String nombre;
+
+    public Alumno(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Alumno otro = (Alumno) obj;
+        return otro.getNombre().equals(this.nombre);
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
